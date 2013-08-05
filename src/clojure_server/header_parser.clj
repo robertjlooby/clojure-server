2efb9d7b839af479cb563eea6b077b783b5f5f92
robertjlooby <robertjlooby@gmail.com>
2013-08-05 16:59:07 -0500

    removed use of transient data structure

--- src/clojure_server/header_parser.clj
+++ src/clojure_server/header_parser.clj
@@ -1,15 +1,14 @@
 (ns clojure_server.header-parser)
 
 (defn parse-headers [scanner]
-  (let [headers (transient {})
-        request-line (clojure.string/split (.nextLine scanner) 
+  (let [request-line (clojure.string/split (.nextLine scanner) 
                                            #" ")]
-    (assoc! headers :method (first request-line))
-    (assoc! headers :path (second request-line))
-    (assoc! headers :http-version (last request-line))
-    (loop [line (.nextLine scanner)]
+    (loop [headers (hash-map :method (first request-line)
+                             :path (second request-line)
+                             :http-version (last request-line))
+           line (.nextLine scanner)]
       (if-not (empty? line) 
         (let [[_, field, value] (re-matches #"(.*):\s+(.*)" line)]
-          (assoc! headers (keyword field) value)
-          (recur (.nextLine scanner)))))
-    (persistent! headers)))
+          (recur (assoc headers (keyword field) value)
+                 (.nextLine scanner)))
+        headers))))
