a69ca4bda99dc37a89c12535b4cdf24dec7712a5
robertjlooby <robertjlooby@gmail.com>
2013-08-06 13:57:10 -0500

    removed unnecessary mapping

--- src/clojure_server/response_builder.clj
+++ src/clojure_server/response_builder.clj
@@ -1,16 +1,15 @@
 (ns clojure_server.response-builder)
 
 (defn build-response [title, content]
-    (map identity
-      ["HTTP/1.1 200 OK"
-       ""
-       "<!DOCTYPE html>"
-       "<html>"
-       "<head>"
-       (str "<title>" title "</title>")
-       "</head>"
-       "<body>"
-       content
-       "</body>"
-       "</html>"
-       ""]))
+  ["HTTP/1.1 200 OK"
+   ""
+   "<!DOCTYPE html>"
+   "<html>"
+   "<head>"
+   (str "<title>" title "</title>")
+   "</head>"
+   "<body>"
+   content
+   "</body>"
+   "</html>"
+   ""])
