851640ba13c4f88a183681e45fea1bd503fdf5dd
robertjlooby <robertjlooby@gmail.com>
2013-08-13 16:08:28 -0500

    changed server to serve from a directory/port from the command line (for cob_spec)

--- src/clojure_server/server.clj
+++ src/clojure_server/server.clj
@@ -1,6 +1,13 @@
 (ns clojure_server.server
-  (:require [clojure_server.core :refer :all]))
+  (:require [clojure_server.core :refer :all])
+  (:use clojure.contrib.command-line)
+  (:gen-class))
 
-(with-open [server-socket (create-server-socket 3000
-                          (java.net.InetAddress/getByName "localhost"))]
-  (echo-server server-socket))
+(defn -main [& args]
+  (with-command-line args
+    "Usage...."
+    [[port p "The port to run the server on" "3000"]
+     [directory d "The directory to serve files from"]]
+  (with-open [server-socket (create-server-socket (read-string port)
+                            (java.net.InetAddress/getByName "localhost"))]
+    (server server-socket directory))))
