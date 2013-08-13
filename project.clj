5000e683196c5f8d33dec75fe58407bba9d4eae8
robertjlooby <robertjlooby@gmail.com>
2013-08-13 16:07:02 -0500

    added clojure.contrib to dependencies for with-command-args

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "An HTTP server implemented in Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :profiles {:dev {:dependencies [[speclj "2.5.0"]]}}
   :plugins [[speclj "2.5.0"]]
   :jvm-opts ["-Dline.separator=\r\n"]
