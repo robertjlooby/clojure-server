9ab5ed02f3911137350ec4fba91ab2694609d714
robertjlooby <robertjlooby@gmail.com>
2013-08-13 22:28:03 -0500

    added ability to match params in path to router

--- spec/clojure_server/router_spec.clj
+++ spec/clojure_server/router_spec.clj
@@ -2,6 +2,46 @@
   (:require [speclj.core :refer :all]
             [clojure_server.router :refer :all]))
 
+(describe "parse-router-path"
+  (it "should return [''] for '/'"
+   (should= [""] (parse-router-path "/")))
+
+  (it "should return ['hello' 'world'] for '/hello/world'"
+    (should= ["hello" "world"] (parse-router-path "/hello/world")))
+
+  (it "should return ['hello' :id] for '/hello/:id'"
+    (should= ["hello" :id] (parse-router-path "/hello/:id")))
+
+  (it "should return ['a' :b :c 'd.html'] for '/a/:b/:c/d.html'"
+    (should= ["a" :b :c "d.html"] (parse-router-path "/a/:b/:c/d.html")))
+)
+
+(describe "params-match"
+  (it "should return nil when not a match"
+      (should= nil (params-match "/" "/hello")))
+
+  (it "should return an empty hash-map when it is a match"
+      (should= {} (params-match "/hello" "/hello")))
+
+  (it "should be able to match full string"
+      (should= {} (params-match "/hello/world/deep/path.html" 
+                                "/hello/world/deep/path.html")))
+
+  (it "should match anything to a string and return it in the hash"
+    (should= {:id "12345"} (params-match "/:id" "/12345")))
+
+  (it "should match multiple params"
+    (should= {:a "12" :b "xy" :c "3Z"} 
+             (params-match "/root/:a/:b/space/:c/page.html" 
+                           "/root/12/xy/space/3Z/page.html")))
+
+  (it "should return nil even when params partially match, router longer"
+    (should= nil (params-match "/:a/url/page" "/1234/url")))
+
+  (it "should return nil even when params partially match, request longer"
+    (should= nil (params-match "/:a/url/page" "/1234/url/page/user")))
+)
+
 (describe "router"
   (it "should be able to match exact route names"
     (defrouter my-router [request params]
@@ -19,4 +59,28 @@
       (GET "/testpath" (generator (:method request) (:path request))))
     (should= ["GET /testpath", 200]
              (my-router {:method "GET" :path "/testpath"})))
+
+  (it "should match a path with a keyword param"
+    (defrouter my-router [request params]
+      (GET "/" ["root" 200])
+      (GET "/:file" ["matched file" 200]))
+    (should= ["root" 200]
+             (my-router {:method "GET" :path "/"}))
+    (should= ["matched file" 200]
+             (my-router {:method "GET" :path "/file1"})))
+
+  (it "should match a path with a multiple keyword params"
+    (defrouter my-router [request params]
+      (GET "/" ["root" 200])
+      (GET "/:file"            ["matched 1 param"  200])
+      (GET "/:file/:user"      ["matched 2 params" 200])
+      (GET "/:file/:user/:num" ["matched 3 params" 200]))
+    (should= ["root" 200]
+             (my-router {:method "GET" :path "/"}))
+    (should= ["matched 1 param" 200]
+             (my-router {:method "GET" :path "/file1"}))
+    (should= ["matched 2 params" 200]
+             (my-router {:method "GET" :path "/file1/rob"}))
+    (should= ["matched 3 params" 200]
+             (my-router {:method "GET" :path "/file1/rob/42"})))
 )
--- src/clojure_server/router.clj
+++ src/clojure_server/router.clj
@@ -1,12 +1,45 @@
 (ns clojure_server.router)
 
+(defn parse-path [path]
+  (let [relative-path (if (= \/ (first path))
+                        (subs path 1)
+                        path)]
+    (clojure.string/split relative-path #"/")))
+
+(defn parse-router-path [router-path]
+  (let [path-vec (parse-path router-path)]
+    (map #(if (= \: (first %))
+           (keyword (subs % 1))
+           %) path-vec)))
+
+(defn params-match [router-path request-path]
+  (loop [router-path-vec (parse-router-path router-path)
+         request-path-vec (parse-path request-path)
+         params {}]
+    (cond
+      (not= (count router-path-vec) (count request-path-vec))
+        nil
+      (and (empty? router-path-vec) (empty? request-path-vec))
+        params
+      (keyword? (first router-path-vec))
+        (recur (rest router-path-vec)
+               (rest request-path-vec)
+               (assoc params (first router-path-vec)
+                             (first request-path-vec)))
+      (= (first router-path-vec) (first request-path-vec))
+        (recur (rest router-path-vec)
+               (rest request-path-vec)
+               params)
+      :else
+        nil)))
+
 (defmacro defrouter [router-name args & routes]
   `(defn ~router-name [~(first args)]
      ~(list* 'cond 
             (apply concat
               (map #(list `(and (= ~(str (first %)) 
                                     (:method ~(first args)))
-                                (= ~(second %) 
+                                (params-match ~(second %) 
                                     (:path ~(first args))))
                            (last %))
                    routes)))))
