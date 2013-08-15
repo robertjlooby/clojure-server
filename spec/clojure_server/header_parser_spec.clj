b368d2156b357b2bfa974339790256dbbbf72339
robertjlooby <robertjlooby@gmail.com>
2013-08-15 10:54:51 -0500

    added :body to headers when request has Content-Length header

--- spec/clojure_server/header_parser_spec.clj
+++ spec/clojure_server/header_parser_spec.clj
@@ -4,8 +4,14 @@
 
 (describe "header parser"
   (with get-seq '("GET /helloworld HTTP/1.1"
+                  "Accept: text/html"
+                  "Connection: keep-alive"
                   ""))
   (with post-seq '("POST /helloworldform HTTP/1.1"
+                   "Content-Length: 10"
+                   ""
+                   "hello"
+                   "world"
                    ""))
 
   (it "should get GET requests"
@@ -27,4 +33,14 @@
   (it "should get HTTP version"
     (let [headers (parse-headers @get-seq)]
       (should= "HTTP/1.1" (:http-version headers))))
+
+  (it "should get all headers"
+    (let [headers (parse-headers @get-seq)]
+      (should= "keep-alive" (:Connection headers))
+      (should= "text/html" (:Accept headers))))
+
+  (it "should get the body when Content-Length present"
+    (let [headers (parse-headers @post-seq)]
+      (should= "10" (:Content-Length headers))
+      (should= '("hello" "world") (:body headers))))
 )
--- src/clojure_server/header_parser.clj
+++ src/clojure_server/header_parser.clj
@@ -2,14 +2,23 @@
 
 (defn parse-headers [in-seq]
   (let [request-line (clojure.string/split (first in-seq) 
-                                           #" ")]
-    (loop [headers (hash-map :method (first request-line)
-                             :path (second request-line)
-                             :http-version (last request-line))
-           lines (rest in-seq)]
-      (if-not (empty? (first lines)) 
-        (let [[_, field, value] (re-matches #"(.*):\s+(.*)" 
-                                            (first lines))]
-          (recur (assoc headers (keyword field) value)
-                 (rest lines)))
-        headers))))
+                                           #" ")
+        [headers rest-lines] 
+                (loop [heads (hash-map :method (first request-line)
+                              :path (second request-line)
+                              :http-version (last request-line))
+                       lines (rest in-seq)]
+                  (if-not (empty? (first lines)) 
+                   (let [[_, field, value] 
+                          (re-matches #"(.*):\s+(.*)" (first lines))]
+                     (recur (assoc heads (keyword field) value)
+                            (rest lines)))
+                     [heads (rest lines)]))]
+    (if (and (:Content-Length headers)
+             (> (read-string (:Content-Length headers)) 0))
+      (loop [body []
+             lines rest-lines]
+        (if-not (empty? (first lines))
+          (recur (conj body (first lines)) (rest lines))
+          (assoc headers :body body)))
+      headers)))
